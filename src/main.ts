fd97e2d95d6e419c9af9259500ba27403a26379d
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2023-08-14 05:34:08 +0900

    Feat: swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
+import { setupSwagger } from './util/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
