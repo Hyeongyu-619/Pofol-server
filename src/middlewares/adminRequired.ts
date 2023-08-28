b479ab076a3d34f98247bd22b7386fe82eb17ba3
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2023-08-28 13:05:20 +0900

    Fix: admin,mentor Required 검증 수정

--- src/middlewares/adminRequired.ts
+++ src/middlewares/adminRequired.ts
@@ -2,22 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import { userService } from "../services/userService";
 
 async function adminRequired(req: any, res: Response, next: NextFunction) {
-  const adminId = req.currentUserId;
+  // req.currentUser 는 loginRequired 미들웨어에서 설정해준 값입니다.
+  const currentUser = req.currentUser;
 
-  if (!adminId) {
-    const error = new Error("유저 ID가 존재하지 않습니다.");
+  if (!currentUser) {
+    const error = new Error("유저 정보가 존재하지 않습니다.");
     error.name = "Unauthorized";
     return next(error);
   }
 
-  const adminInfo = await userService.getUserById(adminId);
-  if (!adminInfo) {
-    const error = new Error("관리자 정보를 찾을 수 없습니다.");
-    error.name = "NotFound";
-    return next(error);
-  }
-
-  if (adminInfo.role !== "admin") {
+  if (currentUser.role !== "admin") {
     const error = new Error("관리자 권한이 없습니다.");
     error.name = "Unauthorized";
     return next(error);
--- src/middlewares/mentorRequired.ts
+++ src/middlewares/mentorRequired.ts
@@ -2,22 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import { userService } from "../services/userService";
 
 async function mentorRequired(req: any, res: Response, next: NextFunction) {
-  const mentorId = req.currentUserId;
+  // req.currentUser 는 loginRequired 미들웨어에서 설정해준 값입니다.
+  const currentUser = req.currentUser;
 
-  if (!mentorId) {
-    const error = new Error("유저 ID가 존재하지 않습니다.");
+  if (!currentUser) {
+    const error = new Error("유저 정보가 존재하지 않습니다.");
     error.name = "Unauthorized";
     return next(error);
   }
 
-  const mentorInfo = await userService.getUserById(mentorId);
-  if (!mentorInfo) {
-    const error = new Error("멘토 정보를 찾을 수 없습니다.");
-    error.name = "NotFound";
-    return next(error);
-  }
-
-  if (mentorInfo.role !== "mentor") {
+  if (currentUser.role !== "mentor") {
     const error = new Error("멘토 권한이 없습니다.");
     error.name = "Unauthorized";
     return next(error);
