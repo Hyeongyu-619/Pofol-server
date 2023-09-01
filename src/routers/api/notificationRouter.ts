f95e58b527b4595bb1105c2c408df846426bafce
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2023-09-01 18:29:54 +0900

    Feat: 내 알림만 가져오기 추가

--- src/routers/api/notificationRouter.ts
+++ src/routers/api/notificationRouter.ts
@@ -4,35 +4,59 @@ import { adminRequired, loginRequired } from "../../middlewares";
 
 const notificationRouter = Router();
 
-notificationRouter.get(
-  "/",
-  loginRequired,
-  async (req: any, res: Response, next: NextFunction) => {
-    try {
-      const limit = Number(req.query.limit) || 5;
-      const skip = Number(req.query.skip) || 0;
+// notificationRouter.get(
+//   "/",
+//   loginRequired,
+//   async (req: any, res: Response, next: NextFunction) => {
+//     try {
+//       const limit = Number(req.query.limit) || 5;
+//       const skip = Number(req.query.skip) || 0;
 
-      const [notifications, total] =
-        await notificationService.findAllNotificationsWithPagination(
-          req.currentUser._id,
-          skip,
-          limit
-        );
+//       const [notifications, total] =
+//         await notificationService.findAllNotificationsWithPagination(
+//           req.currentUser._id,
+//           skip,
+//           limit
+//         );
 
-      const totalPages = Math.ceil(total / limit);
+//       const totalPages = Math.ceil(total / limit);
 
-      res.status(200).json({
-        notifications,
-        total,
-        totalPages,
-        limit,
-        skip,
-      });
-    } catch (error) {
-      next(error);
+//       res.status(200).json({
+//         notifications,
+//         total,
+//         totalPages,
+//         limit,
+//         skip,
+//       });
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
+
+notificationRouter.get("/", async (req, res) => {
+  try {
+    const userId = req.query.userId as string;
+    const skip = parseInt(req.query.skip as string, 10);
+    const limit = parseInt(req.query.limit as string, 10);
+
+    if (!userId || isNaN(skip) || isNaN(limit)) {
+      return res.status(400).json({ error: "Invalid query parameters" });
     }
+
+    const [notifications, total] =
+      await notificationService.findAllNotificationsWithPagination(
+        userId,
+        skip,
+        limit
+      );
+
+    res.status(200).json({ notifications, total });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-);
+});
 
 notificationRouter.delete(
   "/:id",
