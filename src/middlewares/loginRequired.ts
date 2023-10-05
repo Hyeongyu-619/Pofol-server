5dec80fc3ca77525d3605dc786ef3f3cfa69785d
hyeongyu-kim <rlagusrb619@gmail.com>
2023-10-05 21:38:44 +0900

    Fix: 토큰 만료시 메인으로 redirect 처리하게 수정

--- src/middlewares/loginRequired.ts
+++ src/middlewares/loginRequired.ts
@@ -1,5 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { userService } from "../services";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+function isJwtPayloadWithExpiration(
+  token: string | JwtPayload
+): token is JwtPayload & { exp: number } {
+  return typeof token !== "string" && "exp" in token;
+}
 
 async function loginRequired(req: any, res: Response, next: NextFunction) {
   const token = req.cookies.token;
@@ -9,6 +16,21 @@ async function loginRequired(req: any, res: Response, next: NextFunction) {
       result: "Unauthorized",
       reason: "로그인한 유저만 사용할 수 있는 서비스입니다.",
     });
+  }
+  const decodedToken = jwt.decode(token); // verify가 아닌 decode를 사용하여 토큰 내용만 가져옵니다.
+
+  if (!decodedToken) {
+    return res.status(401).json({
+      result: "Unauthorized",
+      reason: "정상적인 토큰이 아닙니다.",
+    });
+  }
+
+  if (
+    isJwtPayloadWithExpiration(decodedToken) &&
+    decodedToken.exp < Date.now() / 1000
+  ) {
+    return res.redirect("/");
   }
 
   try {
